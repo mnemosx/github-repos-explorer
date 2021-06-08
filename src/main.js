ef219adc370f908496e871c96315df098131f249
mnemosx <akacerovskis@inbox.lv>
2021-06-08 19:28:47 +0300

    added fontawesome icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+/* fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch, faTrash, faHeart, faHeartBroken, faStar, faCodeBranch, faCheck, faTimes, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartR, faUserCircle } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faSearch, faTrash, faHeart, faHeartR, faHeartBroken, faStar, faCodeBranch, faCheck, faTimes, faLongArrowAltRight, faUserCircle)
+
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
