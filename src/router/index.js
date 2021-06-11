8ab2baa063916d18a915bfa52f323894bbc61d41
mnemosx <akacerovskis@inbox.lv>
2021-06-11 21:16:06 +0300

    Removing hash (#) from url

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import FourOhFour from '../views/FourOhFour.vue';
 
 const routes = [
   {
@@ -14,11 +15,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/Results.vue')
-  }
+  },
+  {
+    path: '/:pathMatch(.*)',
+    component: FourOhFour
+  },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
